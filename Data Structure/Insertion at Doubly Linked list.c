7d8b957b4fb7dba9a97334a8505cb193cd5bc2bd
Hafijur Rahman Safin <109754142+hafijurrahmansafin24@users.noreply.github.com>
2026-03-01 05:41:26 +0600

    Update Insertion at Doubly Linked list.c

--- Data Structure/Insertion at Doubly Linked list.c
+++ Data Structure/Insertion at Doubly Linked list.c
@@ -1,43 +1,183 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node {
-	int data;
-	struct node* prev;
-	struct node* next;
+struct Node {
+    int data;
+    struct Node* prev;
+    struct Node* next;
 };
 
+struct Node* head = NULL;
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->prev = NULL;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void insertBeginning(int data) {
+    struct Node* newNode = createNode(data);
+
+    if (head == NULL) {
+        head = newNode;
+    } else {
+        newNode->next = head;
+        head->prev = newNode;
+        head = newNode;
+    }
+}
+
+void insertEnd(int data) {
+    struct Node* newNode = createNode(data);
+
+    if (head == NULL) {
+        head = newNode;
+        return;
+    }
+
+    struct Node* temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+    newNode->prev = temp;
+}
+
+void insertAtPosition(int data, int pos) {
+
+    if (pos == 1) {
+        insertBeginning(data);
+        return;
+    }
+
+    struct Node* temp = head;
+
+    for (int i = 1; i < pos - 1 && temp != NULL; i++)
+        temp = temp->next;
+
+    if (temp == NULL) {
+        printf("Position out of bounds.\n");
+        return;
+    }
+
+    struct Node* newNode = createNode(data);
+
+    newNode->next = temp->next;
+    newNode->prev = temp;
+
+    if (temp->next != NULL)
+        temp->next->prev = newNode;
+
+    temp->next = newNode;
+}
+
+void insertAfterValue(int value, int data) {
+
+    struct Node* temp = head;
+
+    while (temp != NULL && temp->data != value)
+        temp = temp->next;
+
+    if (temp == NULL) {
+        printf("Value not found in the list.\n");
+        return;
+    }
+
+    struct Node* newNode = createNode(data);
+
+    newNode->next = temp->next;
+    newNode->prev = temp;
+
+    if (temp->next != NULL)
+        temp->next->prev = newNode;
+
+    temp->next = newNode;
+}
+
+void display() {
+    struct Node* temp = head;
+
+    printf("Current Doubly Linked List: ");
+
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    printf("\n");
+}
+
 int main() {
-	struct node *head = NULL, *temp, *newNode;
-	int n;
 
-	printf("Enter number of nodes: ");
-	scanf("%d", &n);
+    int n, data, pos, value, choice;
 
-	for(int i = 0; i < n; i++) {
-		newNode = (struct node*)malloc(sizeof(struct node));
-		printf("Enter data: ");
-		scanf("%d", &newNode -> data);
-		newNode -> prev = NULL;
-		newNode -> next = NULL;
+    printf("Enter number of elements already in the list: ");
+    scanf("%d", &n);
 
-		if(head == NULL) {
-			head = newNode;
-		}else {
-			temp = head;
-			while(temp -> next != NULL)
-				temp = temp -> next;
-			temp -> next = newNode;
-			newNode -> prev = temp;
-		}
-	}
+    for (int i = 0; i < n; i++) {
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &data);
+        insertEnd(data);
+    }
 
-	printf("Doubly Linked List:\n");
-	temp = head;
-	while(temp != NULL) {
-		printf("%d ", temp -> data);
-		temp = temp -> next;
-	}
+    display();
 
-	return 0;
+    while (1) {
+
+        printf("\nInsertion Menu:\n");
+        printf("1. Insert at Beginning\n");
+        printf("2. Insert at End\n");
+        printf("3. Insert at Position\n");
+        printf("4. Insert After a Value\n");
+        printf("5. Exit\n");
+
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+
+            case 1:
+                printf("Enter data to insert: ");
+                scanf("%d", &data);
+                insertBeginning(data);
+                display();
+                break;
+
+            case 2:
+                printf("Enter data to insert: ");
+                scanf("%d", &data);
+                insertEnd(data);
+                display();
+                break;
+
+            case 3:
+                printf("Enter position: ");
+                scanf("%d", &pos);
+                printf("Enter data to insert: ");
+                scanf("%d", &data);
+                insertAtPosition(data, pos);
+                display();
+                break;
+
+            case 4:
+                printf("Enter value after which to insert: ");
+                scanf("%d", &value);
+                printf("Enter data to insert: ");
+                scanf("%d", &data);
+                insertAfterValue(value, data);
+                display();
+                break;
+
+            case 5:
+                printf("Exiting program...\n");
+                exit(0);
+
+            default:
+                printf("Invalid choice.\n");
+        }
+    }
+
+    return 0;
 }
